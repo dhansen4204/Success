7ecce09f97ac869c18b3c03c0c5672d473d9a76f
dhansen4204 <34971844+dhansen4204@users.noreply.github.com>
2020-03-21 17:04:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
       botRegexRip = /^\/rip/; botRegexCrrct = /^\/correct/;
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexVotes = /^\/votes/; botRegexGoodg = /^\/gg/;
-      botRegexLaugh = /^\/lol/; botRegexWrong = /^\/wrong/; botRegexShaki = /^\/shaki/;
+      botRegexLaugh = /^\/lol/; botRegexWrong = /^\/wrong/; botRegexDAL = /^\/dl/;
       botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -63,15 +63,14 @@ function respond() {
     postMessage("https://i.groupme.com/480x287.gif.987cf3955af443d0a30ccbfbec2d30b4");
     this.res.end();
   } 
-  else if(request.text && botRegexShaki.test(request.text)) {
+  else if(request.text && botRegexDAL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://i.groupme.com/305x375.jpeg.a8dd3aa18c7a4963a553e6dc16bcb100.large");
+    postMessage("https://www.daddyleagues.com/success");
     this.res.end();
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("https://www.daddyleagues.com/success");
+    postMessage("http://www.daddyleagues.com/success/maddenrating?name=&position=all&team="+request.text.substring(5,8));
     this.res.end();
   } 
   
